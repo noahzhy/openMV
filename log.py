7d2ebd30668aa63da9f74c555738b32e68497ee9
noahzhy <noahzhy@gmail.com>
2020-01-23 17:37:06 +0800

    add config.ini

--- log.py
+++ log.py
@@ -1,10 +1,14 @@
 import os
 import csv
 import Class
+import configparser
 
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 def add_log(obj=Class.FallLog()):
-    log_file_name = 'log.csv'
+    log_file_name = config.get('log', 'log_file_name')
     file_path = os.path.join(os.getcwd(), log_file_name)
     header = ['timestamp', 'content', 'post_status', 'address']
     if not os.path.isfile(file_path):
@@ -15,6 +19,8 @@ def add_log(obj=Class.FallLog()):
     with open(file_path, 'a+', newline='') as f:
         write = csv.DictWriter(f, obj.get_dict().keys())
         write.writerow(obj.get_dict())
+
+    return True
 
 
 if __name__ == "__main__":
--- messenger.py
+++ messenger.py
@@ -1,2 +1,23 @@
 import csv
 import os
+import log
+import Class
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+first_running = config.getboolean('first', 'first_running')
+debug_mode = config.getboolean('debug', 'debug_mode')
+
+def read_log():
+    if first_running:
+        obj = Class.FallLog()
+        obj.set_content('first running')
+        if log.add_log(obj) and not debug_mode:
+            config.set('first', 'first_running', 'False')
+            config.write(open('config.ini', 'w'))
+
+
+if __name__ == "__main__":
+    read_log()
