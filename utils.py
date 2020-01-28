4157d7c73ac095da5b2071b11a211a2ff696c229
noahzhy <noahzhy@gmail.com>
2020-01-28 23:47:56 +0800

    add ensure_dir function

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ def find_new_version(current_version, server_version):
     return True if current_version < server_version else False
 
 
+def ensure_folder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
 if __name__ == "__main__":
     server_version = '2.1.20'
     a = '2.1.20'
